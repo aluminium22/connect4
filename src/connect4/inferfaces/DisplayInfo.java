3682c0f9cf037a5898b847dd5f4cb35014f8c91b
Aluminium <Aluminium@192.168.0.3>
2014-03-22 14:12:27 -0600

    added classes/ints

---
+++ src/connect4/inferfaces/DisplayInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4.inferfaces;
+
+/**
+ *
+ * @author Aluminium
+ */
+public interface DisplayInfo {
+    void display();
+    
+}
